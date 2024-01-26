77e0ef4487b7cfde42c10e16125dc67620ad4d97
Fabio Soares <soaresventurellifabio@gmail.com>
2024-01-26 13:53:35 -0300

    feat: add method to weather by city

--- lib/weather_sdk.rb
+++ lib/weather_sdk.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 require_relative "weather_sdk/version"
+require 'json'
+require 'httparty'
 
 module WeatherSdk
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Client
+    include HTTParty
+    API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+    def self.get_weather_by_city(city_name, api_key)
+      url = "#{API_URL}?q=#{city_name}&appid=#{api_key}"
+      response = get(url)
+
+      if response.code == 200
+        response_parsed = JSON.parse(response.body)
+        response_parsed.dig('main', 'temp')
+      else
+        raise Error, 'Cannot find city or execute request'
+      end
+    end
+  end
 end
